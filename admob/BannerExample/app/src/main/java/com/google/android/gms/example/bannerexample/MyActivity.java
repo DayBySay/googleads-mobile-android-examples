d0701f3148a638dff4fdcaa22d70446c3dfe5039
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-10-03 13:20:48 +0900

    実装の修正

--- admob/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
+++ admob/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
@@ -20,8 +20,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
 import com.google.android.gms.ads.MobileAds;
 
 /**
@@ -29,7 +29,7 @@ import com.google.android.gms.ads.MobileAds;
  */
 public class MyActivity extends ActionBarActivity {
 
-    private AdView mAdView;
+    private PublisherAdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +41,13 @@ public class MyActivity extends ActionBarActivity {
 
         // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
         // values/strings.xml.
-        mAdView = (AdView) findViewById(R.id.ad_view);
+        mAdView = (PublisherAdView) findViewById(R.id.ad_view);
 
         // Create an ad request. Check your logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+                .addTestDevice(PublisherAdRequest.DEVICE_ID_EMULATOR)
                 .build();
 
         // Start loading the ad in the background.
